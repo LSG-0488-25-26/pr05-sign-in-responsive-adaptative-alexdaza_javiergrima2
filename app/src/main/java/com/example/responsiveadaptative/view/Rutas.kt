fc1b0c55002fff83df0a87fec49ec989e8f0fb50
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2025-12-18 16:50:01 +0100

    Crear archivo con las rutas de las diferentes pantallas

--- app/src/main/java/com/example/responsiveadaptative/view/Rutas.kt
+++ app/src/main/java/com/example/responsiveadaptative/view/Rutas.kt
@@ -1,2 +1,7 @@
 package com.example.responsiveadaptative.view
 
+sealed class Routes(val route: String)
+{
+    object CrearCuenta:Routes("CrearCuenta" )
+    object IniciarSesion:Routes("IniciarSesion" )
+}
